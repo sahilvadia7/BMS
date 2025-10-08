0ea9173488ba22dbbc36397fd9f732fa1a3f49ef
sahil vadia <sahilvadia7776@gmail.com>
2025-10-08 16:08:27 +0530

    all service created

---
+++ branch-service/src/main/java/com/bms/branch/BranchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bms.branch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchServiceApplication.class, args);
+	}
+
+}
---
+++ eureka-server/src/test/java/com/bms/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bms.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
